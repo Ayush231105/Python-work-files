34ba328a8534e786e6f412960943818498429388
Ayush231105 <mukherjeeayush664@gmail.com>
2025-08-04 19:09:55 +0530

    Create A-M's p5.py

---
+++ A-M's p5.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+add = num1 + num2
+mul = num1 * num2
+sub = num1 - num2
+div = num1 / num2
+
+print ("The addition is", add)
+print ("The Multiplication is", mul)
+print ("The Substraction is", sub)
+print ("The Division is", div)
