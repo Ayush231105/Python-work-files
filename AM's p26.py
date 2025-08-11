127a60d9d7ff43ecffc596deebfa2afa953645df
Ayush231105 <mukherjeeayush664@gmail.com>
2025-08-11 19:54:48 +0530

    Update AM's p26.py

--- AM's p26.py
+++ AM's p26.py
@@ -1,6 +1,9 @@
+1. check if the words are panagrom or not
+
 word1 = input("Enter the first word: ")
 word2 = input("Enter the second word: ")
 if sorted(word1) == sorted(word2):
     print("The words are anagrams.")
 else:
+
     print("The words are not anagrams.")
