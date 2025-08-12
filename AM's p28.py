cf824e307699b7aa99be3e9ff8c77a3973a08e50
Ayush231105 <mukherjeeayush664@gmail.com>
2025-08-12 20:18:09 +0530

    Add files via upload

---
+++ AM's p28.py
@@ -0,0 +1,8 @@
+num = int(input("Enter your marks: "))
+
+if num >= 75:
+    print("You have passed with distinction.")
+elif num >= 40:
+    print("You have passed the exam.")
+else:
+    print("You have failed the exam.")
